5db43c5deb75a1d41ec4682e6c2be91640d8c5f9
fekoerbel <fekoerbel@hotmail.com>
2025-03-18 23:15:41 -0300

    refactor backend structure: remove unused files and implement new database modules for projects and technologies

---
+++ backend/src/projeto/projeto.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Param } from "@nestjs/common";
+import { ProjetoPrisma } from "./projeto.prisma";
+import { Projeto } from "@core";
+
+@Controller("projetos")
+export class ProjetoController {
+	constructor(private readonly repo: ProjetoPrisma) {}
+	@Get()
+	async obterTodos(): Promise<Projeto[]> {
+		return this.repo.obterTodos();
+	}
+
+	@Get("destaques")
+	async obterDestaques(): Promise<Projeto[]> {
+		return this.repo.obterDestaques();
+	}
+
+	@Get(":id")
+	async obterPorId(@Param("id") id: string): Promise<Projeto | null> {
+		return this.repo.obterPorId(Number(id));
+	}
+}
