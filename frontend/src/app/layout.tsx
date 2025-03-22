a8de0b11736e0f54435bad692fa023069c7e9239
fekoerbel <fekoerbel@hotmail.com>
2025-03-21 21:00:39 -0300

    feat: frontend - First commit

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Portfólio",
+  description: "Portfólio de desenvolvimento usando agente de IA",
+};
+
+const fonte = Montserrat({
+  subsets: ["latin",]
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${fonte.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
